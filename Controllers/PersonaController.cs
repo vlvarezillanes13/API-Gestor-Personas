1dc82f9332f4c196f3718bcf3e7b8b7bc286a66a
Vicente Álvarez <v.lvarez.illanes@gmail.com>
2023-03-18 01:02:34 -0300

    subida y bajada de archivos

--- Controllers/PersonaController.cs
+++ Controllers/PersonaController.cs
@@ -35,7 +35,7 @@ namespace ApiGestionPersonas.Controllers
         public async Task<IActionResult> GetPersona(int id)
         {
             PersonaEntity? result = await _personaDataBaseContext.Get(id);
-            if(result == null)
+            if (result == null)
                 return new NotFoundResult();
             return new OkObjectResult(result.ToDto());
         }
@@ -45,7 +45,7 @@ namespace ApiGestionPersonas.Controllers
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> GetPersonaByRutDv(string rut, string dv)
         {
-            PersonaEntity? result = await _personaDataBaseContext.GetbyRutDv(rut,dv);
+            PersonaEntity? result = await _personaDataBaseContext.GetbyRutDv(rut, dv);
             if (result == null)
                 return new NotFoundResult();
             return new OkObjectResult(result.ToDto());
@@ -84,6 +84,63 @@ namespace ApiGestionPersonas.Controllers
                 return new NotFoundResult();
 
             return new OkObjectResult(result);
+        }
+
+        [HttpPost("SubirArchivo/{Rut}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> GuardarArchivo(IFormFile archivo, string Rut)
+        {
+            if (archivo == null || archivo.Length == 0)
+            {
+                return BadRequest(false);
+            }
+
+            var rutaArchivo = Path.Combine(Directory.GetCurrentDirectory(), "Archivos", $"DATA_{Rut}.pdf");
+
+            using (var stream = new FileStream(rutaArchivo, FileMode.Create))
+            {
+                await archivo.CopyToAsync(stream);
+            }
+
+            return Ok(true);
+        }
+
+        [HttpGet("ObtenerArchivo/{Rut}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> DescargarArchivo(string Rut)
+        {
+            try
+            {
+                string rutaArchivo = "./Archivos/DATA_" + Rut + ".pdf";
+
+                if (!System.IO.File.Exists(rutaArchivo))
+                {
+                    return NotFound();
+                }
+
+                var stream = await System.IO.File.ReadAllBytesAsync(rutaArchivo);
+
+                if (stream == null)
+                {
+                    return NotFound();
+                }
+
+                var contentType = "application/pdf";
+
+                return File(stream, contentType, $"DATA_{Rut}.pdf");
+            }
+            catch
+            {
+                return BadRequest();
+            }
+            finally
+            {
+                Dispose();
+            }
+
         }
     }
 }
